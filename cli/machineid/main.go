9feac1371737af46e59dabd4af55a8572779e015
Denis Brodbeck <denis@shahaya.com>
2017-10-19 16:45:59 +0200

    Modify cli app for raw id and hashed id modes.

--- cli/machineid/main.go
+++ cli/machineid/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/denisbrodbeck/machineid"
 )
 
+const usageStr = `
+Usage: machineid [options]
+
+Options:
+  --appid    <AppID>    Protect machine id by hashing it together with an app id.
+
+Try:
+  machineid
+  machineid --appid MyAppID
+`
+
+func usage() {
+	log.Fatalln(usageStr)
+}
+
 func main() {
+	var appID string
+	flag.StringVar(&appID, "appid", "", "Protect machine id by hashing it together with an app id.")
+
 	log.SetFlags(0)
-	id, err := machineid.ID()
+	flag.Usage = usage
+	flag.Parse()
+
+	var id string
+	var err error
+	if appID != "" {
+		id, err = machineid.ProtectedID(appID)
+	} else {
+		id, err = machineid.ID()
+	}
 	if err != nil {
 		log.Fatalf("Failed to read machine id with error: %s\n", err)
 	}
